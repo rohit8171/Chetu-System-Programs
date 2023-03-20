747c4f01046ec4129d11a6c7f4c746ac8ac46a80
ROHIT TOMAR <126895828+rohit8171@users.noreply.github.com>
2023-03-20 20:14:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Employee
+{
+    static void Main(string[] args)
+    {
+
+    private int empNo;
+    private string empName;
+    private double empSal;
+
+    public void SetEmpNo(int eno)
+    {
+        empNo = eno;
+    }
+
+    public void SetEmpName(string ename)
+    {
+        empName = ename;
+    }
+
+    public void SetEmpSal(double esal)
+    {
+        empSal = esal;
+    }
+	
+	
+	public int GetEmpNo()
+    {
+        return empNo;
+    }
+    public string GetEmpName()
+    {
+        return empName;
+    }
+    public double GetEmpSal()
+    {
+        return empSal;
+    }
+}}
